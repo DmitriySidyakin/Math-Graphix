c399e086089bc72a768480ff436086c4340c31b3
Dmitriy Sidyakin <sidyakind@gmail.com>
2021-12-26 16:04:34 +0300

    Update MainWindow.xaml.cs

--- MathGraphix-Solution/MathGraphix/MainWindow.xaml.cs
+++ MathGraphix-Solution/MathGraphix/MainWindow.xaml.cs
@@ -25,11 +25,15 @@ namespace MathGraphix
             InitializeComponent();
 
             this.WindowState = WindowState.Maximized;
-        }
 
+            StackPanel_Graph.Background = Brushes.AntiqueWhite;
+        }
+        
         private void MenuItem_Draw_Click(object sender, RoutedEventArgs e)
         {
-            StackPanel_Graph.Background = Brushes.DarkKhaki;
+            SolidColorBrush[] colorBrushValues = new SolidColorBrush[] { Brushes.AliceBlue, Brushes.AntiqueWhite, Brushes.Wheat, Brushes.Coral, Brushes.Red, Brushes.Yellow };
+            Random random = new Random();
+            StackPanel_Graph.Background = (SolidColorBrush)colorBrushValues[random.Next(colorBrushValues.Length)];
         }
     }
 }
